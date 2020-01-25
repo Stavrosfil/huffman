d1ea175bbc581fe52bc3fbb6d7fb4dad99e654d4
Stavros Filosidis <stavrosfil@gmail.com>
2020-01-25 15:28:53 +0200

    Adjust color and font size

--- huff.py
+++ huff.py
@@ -50,6 +50,7 @@ def plot(code, chars):
     x = np.arange(len(chars))
     # The width of the bars
     width = 0.35
+    font_size = 15
 
     fig, ax1 = plt.subplots()
     ax2 = ax1.twinx()
@@ -67,17 +68,20 @@ def plot(code, chars):
     # The two bars in the plot
     # Original character count
     rects1 = ax1.bar(x - width / 2, char_freq, width, label='Original distribution', color='salmon')
-    ax1.set_ylabel('Frequency', color='salmon')
+    ax1.set_ylabel('Frequency', color='salmon', size=font_size)
+    ax1.tick_params(axis='y', labelcolor='salmon', labelsize=font_size/1.25)
 
     # Huffman encoded count
-    rects2 = ax2.bar(x + width / 2, char_encoded_len, width, label='Huffman code length', color='lightseagreen')
-    ax2.set_ylabel('Encoded Length', color='lightseagreen')
+    rects2 = ax2.bar(x + width / 2, char_encoded_len, width,
+                     label='Huffman code length', color='lightseagreen')
+    ax2.set_ylabel('Encoded Length', color='lightseagreen', size=font_size)
+    ax2.tick_params(axis='y', labelcolor='lightseagreen', labelsize=font_size/1.25)
 
     # Add some text for labels, title and custom x-ax1is tick labels, etc.
-    ax1.set_title('Distribution of original and encoded character length after Huffman code')
-    ax1.set_xlabel('Character')
+    ax1.set_title('Distribution of original and encoded character length after Huffman code', size=font_size)
+    ax1.set_xlabel('Character', size=font_size)
     ax1.set_xticks(x)
-    ax1.set_xticklabels(labels)
+    ax1.set_xticklabels(labels, size=font_size/1.25)
 
     fig.legend()
     plt.show()
